44f7b5106fa798c32fc404bea530663632710266
Elimar Abreu <elimardeabreu4@hotmail.com>
2017-11-24 23:34:54 -0300

    Test Object Main

--- SO_tp1/src/Main.scala
+++ SO_tp1/src/Main.scala
@@ -1,12 +1,11 @@
 object Main{
 
   def main(args :Array[String]){
-      println("Executando a main");
-  val r = scala.util.Random;
-  var a=0;
-    for( a <- 1 to 10){
-      println(r.nextInt(100));
-
-    }
+      Menu
+      /*val r = scala.util.Random;
+      var a=0;
+      for( a <- 1 to 10){
+        println(r.nextInt(100));
+      }*/
   }
 }
