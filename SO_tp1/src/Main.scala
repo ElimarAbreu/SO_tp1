64ebe38493f39ca2461043fbb75d425b6816ca58
Elimar Abreu <elimardeabreu4@hotmail.com>
2017-11-24 21:23:06 -0300

    first commit

---
+++ SO_tp1/src/Main.scala
@@ -0,0 +1,12 @@
+object Main{
+
+  def main(args :Array[String]){
+      println("Executando a main");
+  val r = scala.util.Random;
+  var a=0;
+    for( a <- 1 to 10){
+      println(r.nextInt(100));
+
+    }
+  }
+}
