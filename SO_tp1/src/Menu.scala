62b6c47a54592ba23228efe39fb021f3c494725d
Elimar Abreu <elimardeabreu4@hotmail.com>
2017-12-04 10:47:28 -0300

    Correção no "Menu"

--- SO_tp1/src/Menu.scala
+++ SO_tp1/src/Menu.scala
@@ -1,11 +1,17 @@
 object Menu {
 
+      var a=0;
+      var b=0;
+      var c=0;
+      var d=0;
+      var e=0;
+      
       //Funções com as opções do menu.
       def tiposistema{
         println("Informe o tipo de sistema a ser simulado:");
         println("1 - Sistemas Batch");
         println("2 - Time Sharing");
-        val a = scala.io.StdIn.readInt();
+        a = scala.io.StdIn.readInt();
         if(a!=1 && a!=2){
           erro
           tiposistema
@@ -16,8 +22,8 @@ object Menu {
          println("Informe os recursos do sistema a ser simulado:");
          println("1 - Monoprogramado");
          println("2 - Multiprogramado");
-         val b = scala.io.StdIn.readInt();
-         if(b==2){
+         b = scala.io.StdIn.readInt();
+         if(b==2){  //Se o sistema escolhido for multiprogramado, mostra a opçao prempção. 
            prempcao
          }
          else if(b!=1 && b!=2){
@@ -30,7 +36,7 @@ object Menu {
          println("Informe uma opção:");
          println("1 - Preemptivo");
          println("2 - Cooperativo(não premptivo)");
-         val c = scala.io.StdIn.readInt();
+         c = scala.io.StdIn.readInt();
          if(c!=1 && c!=2){
            erro
            prempcao
@@ -40,7 +46,7 @@ object Menu {
          println("Informe a quantidade de processadores:");
          println("1 - 1 processador");
          println("2 - 2 processadores");
-         val d = scala.io.StdIn.readInt();
+         d = scala.io.StdIn.readInt();
          if(d!=1 && d!=2){
            erro
            processamento
@@ -48,11 +54,16 @@ object Menu {
       }
       def algoritmos{
          println("Informe o algoritmo de escalonamento a ser utilizado:");
-         println("1 -  First-In First-Out (FIFO)");
-         //println("2 - Shortest Job First");
-         //println("3 - Round Robin");
-         val e = scala.io.StdIn.readInt();
-         if(e!=1){
+         if(c==1){  //Opções para sistemas premptivo.
+           println("1 - Round Robin"); 
+           println("2 - Prioridades"); 
+         }
+         if(c==2 || b==1){  //Opções para sistemas não premptivo(cooperativo).
+           println("1 -  First-In First-Out (FIFO)");
+           println("2 - Shortest Job First");
+         }
+         e = scala.io.StdIn.readInt();
+         if(e!=1 && e!=2){
            erro
            algoritmos
          }
@@ -65,7 +76,7 @@ object Menu {
         println("Opção inválida!");
       }
 
-      //chamada das funções do menu.
+      //Chamada das funções do menu.
       println("Sistema Operacional Fictício");
       tiposistema
       recursosistema
